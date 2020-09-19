4d13520a9c7fc01bbdc1191253bd0f2b5706c2f3
Jefferson Oliveira <jefferoidtex7@gmail.com>
2020-09-19 13:59:36 -0300

    Base projeto

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,6 @@
 window.onload = function(){
+
     var clickObjeto = document.getElementById('calculator').querySelectorAll('.num');
-    
     clickObjeto.forEach(numberClick);
 
 
@@ -13,21 +13,56 @@ window.onload = function(){
         }); 
     }
 
+    //separa os numeros das sinais de somas e quarda o valor da soma
+    function display(displayNumber){
+        
+        var item = displayNumber.innerText;
+        var saveDisplay = document.getElementById('display').value += item;
+        var saveValue = parseInt(saveDisplay);
+        console.log(saveValue);
+        
+        
+        if(item == '-'){
+            alert('subtraction');
+        }
+
+        else if(item == '+'){
+            alert('addition');
+        }
+
+        else if(item == '*'){
+            alert('multiply');
+        }
+
+        else if(item == '/'){
+            alert('share');
+        }
+
+        else if(item == '='){
+            alert('result');
+        }
+
+        else if(item == 'C'){
+            document.getElementById('display').value = "";
+        }
+
+        else{
+            
+        }
+    }
+
+    //indentifica o tipo de calculo e faz o calculo
     function calc(typeCalc){
         if(typeCalc == '+'){
-            result = num1 + num2;
             calculate(result);
         }
         else if(typeCalc == '*'){
-            result = num1 * num2;
             calculate(result);
         }
         else if(typeCalc == '/'){
-            result = num1 / num2;
             calculate(result);
         }
         else if(typeCalc == '-'){
-            result = num1 - num2;
             calculate(result);
         }
         else{
@@ -35,36 +70,8 @@ window.onload = function(){
         }
     }
 
+    //exibe no display o resultado
     function calculate(result){
         document.getElementById('display').value = result;
-    }
-
-    function display(displayNumber){
-        
-        var item = displayNumber.innerText;
-        document.getElementById('display').value += item;
-
-
-        if(item == '-'){
-            alert('subtração');
-        }
-        else if(item == '+'){
-            alert('adição');
-        }
-        else if(item == '*'){
-            alert('multiplicar');
-        }
-        else if(item == '/'){
-            alert('dividir');
-        }
-        else if(item == 'C'){
-            alert('limpar');
-        }
-        else if(item == '='){
-            alert('somar');
-        }
-        else{
-            alert('numero');
-        }
     }
 }
